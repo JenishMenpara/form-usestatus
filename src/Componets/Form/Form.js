581bd5708f545f73a9ada184846ca60138f5c621
jenish <jenish@m.com>
2022-01-27 15:00:06 +0530

    comp

--- src/Componets/Form/Form.js
+++ src/Componets/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./form.css";
 
 export default function Form() {
@@ -7,27 +7,69 @@ export default function Form() {
     lastName: "",
     age: "",
     address: "",
+    photo: "",
   });
   const handeliput = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     setgetoutput({ ...getoutput, [name]: value });
+    validation(name, value);
+    
+  /*   let files = e.target.files;
+    let reader= new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = (r) =>{
+      //console.log("imgae", r.target.result)
+      setgetoutput({...getoutput,photo: r.target.result})
+    } */ 
+    setgetoutput({...getoutput,photo: e.target.files[0]})
+    
   };
 
-
-
+  const inputref = useRef("");
 
   const [newdata, setnewdata] = useState([]);
-  
   const output = () => {
-    const data = { ...getoutput, id: new Date().getTime().toString() };
+    const data = { ...getoutput };
     console.log(newdata);
     setnewdata([...newdata, data]);
-
     setgetoutput({ firstName: "", lastName: "", age: "", address: "" });
+    //setimage()
   };
 
+  const [firstnameE, setFirstnameE] = useState("");
+  const [lastnameE, setLastnameE] = useState("");
+  const [ageE, setAgeE] = useState("");
+  const [addressE, setAddressE] = useState("");
 
+  const validation = (name, value) => {
+    switch (name) {
+      case "firstName":
+        let a = /^[A-Za-z]{0,20}$/.test(value)
+          ? ""
+          : "use charcater and max 20 charcater use";
+        setFirstnameE(a);
+        break;
+
+      case "lastName":
+        let c = /^[A-Za-z]{0,20}$/.test(value)
+          ? ""
+          : "use charcater and max 20 charcater use";
+        setLastnameE(c);
+        break;
+
+      case "age":
+        setAgeE(
+          value >= 10 && value <= 100 ? "" : "minimum 10 and max 100 year"
+        );
+        break;
+
+      case "address":
+        let x = value.length <= 35 ? "" : "only charcater allowed 35";
+        setAddressE(x);
+        break;
+    }
+  };
 
   return (
     <>
@@ -40,6 +82,7 @@ export default function Form() {
             onChange={handeliput}
             type="text"
           ></input>
+          <p>{firstnameE}</p>
         </div>
         <div className="input-box">
           <label>Last Name</label>
@@ -49,6 +92,7 @@ export default function Form() {
             onChange={handeliput}
             type="text"
           ></input>
+          <p>{lastnameE}</p>
         </div>
         <hr />
         <div className="age-box">
@@ -59,6 +103,7 @@ export default function Form() {
             onChange={handeliput}
             type="number"
           ></input>
+          <p>{ageE}</p>
         </div>
         <hr />
         <div className="textaddress">
@@ -68,33 +113,44 @@ export default function Form() {
             name="address"
             onChange={handeliput}
           ></textarea>
+          <p>{addressE}</p>
         </div>
         <hr />
         <div className="sub">
           <input
             type="file"
-            id="myFile"
-            name="filename" /* value={} name="" onChange={} */
+            name="file"
+            id="file"
+            accept="image/*"
+            // onChange={handeliput}
+            onChange={handeliput}
+            ref={inputref}
           />
           <br />
           <button name="" onClick={output} type="button" className="submitbtn">
-            
             Submit
           </button>
         </div>
       </form>
 
-      <div>{newdata.map((e) => {
-          return(
-            <div>
-              <p>{e.firstName}</p>
-              <p>{e.lastName}</p>
-              <p>{e.age}</p>
-              <p>{e.address}</p>
+      <div>
+        {newdata.map((e) => {
+          return (
+            <div className="card">
+              <p className="photo">
+                <img  src={URL.createObjectURL(e.photo)}></img>
+              </p>
+              <p className="firsname">
+                {e.firstName} {e.lastName}
+              </p>
+              <p className="age">
+                {e.age} {"year old"}
+              </p>
+              <p className="address">{e.address}</p>
             </div>
-          )
-
-      })}</div>
+          );
+        })}
+      </div>
     </>
   );
 }
